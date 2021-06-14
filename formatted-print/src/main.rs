c9dc3ceffc1331d029104b8bc0342a4d881fc4f4
stanislaw8 <pavlovinthesky@gmail.com>
2021-06-14 20:22:56 +0300

    formatted print

--- formatted-print/src/main.rs
+++ formatted-print/src/main.rs
@@ -9,4 +9,11 @@ fn main() {
             subj="quick fox",
             verb="jumps over"
             );
+    println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
+    println!("{number:>width$}", number=1, width=15);
+    println!("{number:>0width$}", number=1, width=20);
+    #[allow(dead_code)]
+    struct Structure(i32);
+    //let y = Structure(3);
+    println!("This struct '{}' will print...", 3i32);
 }
