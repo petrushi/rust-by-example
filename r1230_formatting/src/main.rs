467909da201321c86d2ff17eaa89b97d0b8ebb67
stanislaw8 <pavlovinthesky@gmail.com>
2021-06-23 02:04:22 +0300

    formatting

--- r1230_formatting/src/main.rs
+++ r1230_formatting/src/main.rs
@@ -25,7 +25,7 @@ struct Color {
 
 impl Display for Color {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "RGB({red}, {green}, {blue}) 0x{red:02x}{green:02x}{blue:02x}",
+        write!(f, "RGB({red}, {green}, {blue}) 0x{red:02X}{green:02X}{blue:02X}",
         red = self.red,
         green = self.green,
         blue = self.blue)
