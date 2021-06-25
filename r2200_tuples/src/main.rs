7c84e385276290d4e4b9c11ced81ff2933d44c80
stanislaw8 <pavlovinthesky@gmail.com>
2021-06-25 18:07:58 +0300

    tuples, matrix transpose

--- r2200_tuples/src/main.rs
+++ r2200_tuples/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[allow(dead_code)]
 fn reverse(pair: (i32, bool)) -> (bool, i32) {
     let (integer, boolean) = pair;
@@ -5,15 +7,52 @@ fn reverse(pair: (i32, bool)) -> (bool, i32) {
     (boolean, integer)
 }
 
+fn reverse_no_tuple(x: i32, y: bool) -> (bool, i32) {
+    (y, x)
+}
+
+static FIRST: i32 = 355;
+static SECOND: bool = true;
+
 #[derive(Debug)]
 struct Matrix(f32, f32, f32, f32);
 
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "({} {})\n({} {})", self.0, self.1, self.2, self.3)
+    }
+}
+
+//activity
+fn transpose(matrix: Matrix) -> Matrix {
+    Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
+}
 fn main() {
     let long_tuple = (
-        1u8, 2u16, 3u32, 4u64,
-        -1i8, -2i16, -3i32, -4i64,
-        0.1f32, 0.2f64,
-        'a', true,
+        1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, "a", true,
     );
-    println!("long tuple's first value: {}", long_tuple.0);
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple second value: {}", long_tuple.1);
+
+    let result = reverse_no_tuple(FIRST, SECOND);
+    println!("{:?}, {} {}", result, result.0, result.1);
+
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+    // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    // println!("too long tuple: {:?}", too_long_tuple);
+
+    let pair = (1, true);
+    println!("pair is {:?}", pair);
+    println!("one element tuple: {:?}", (5u32,));
+    println!("just an integer: {:?}", (5u32));
+
+    let tuple = (1, "hello", 4.5, (true,));
+    let (a, b, c, d) = tuple;
+    println!("{}, {}, {}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
+    println!("{}", matrix);
+    println!("{}", transpose(matrix));
 }
